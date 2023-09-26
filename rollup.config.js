c9ef426a17e6731915e3b9ed01a0f2475db20abb
FlavioZanoni <flaviozg888@gmail.com>
2023-09-25 21:36:07 -0300

    postcss plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,17 @@
 import dts from "rollup-plugin-dts"
 import esbuild from "rollup-plugin-esbuild"
+import postcss from "rollup-plugin-postcss"
 
 export default [
   {
     input: "src/index.ts",
-    plugins: [esbuild()],
+    plugins: [
+      esbuild(),
+      postcss({
+        plugins: [],
+        inject: true,
+      }),
+    ],
     output: [
       {
         dir: "dist",
