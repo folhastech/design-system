cf8426586b0cec19f25e0dd3f55711855dc496bb
FlavioZanoni <flaviozg888@gmail.com>
2023-10-02 20:39:50 -0300

    Rollback to rollup

--- index.ts
+++
@@ -1,7 +0,0 @@
-import "./src/index.css"
-
-export * from "./src/Button"
-export * from "./src/Drawer"
-export * from "./src/StepForm"
-export * from "./src/inputFields"
-export * from "./src/radixStyled"
