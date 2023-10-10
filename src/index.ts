e99bdf02c3054acf0363f87b6c59aecc5aaf9d78
FlavioZanoni <flaviozg888@gmail.com>
2023-10-10 20:15:00 -0300

    Exportando todos os hooks

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export * from "./Button"
 export * from "./Drawer"
 export * from "./StepForm"
-export * from "./hooks/useSize"
+export * from "./hooks"
 export * from "./inputFields"
 export * from "./radixStyled"
