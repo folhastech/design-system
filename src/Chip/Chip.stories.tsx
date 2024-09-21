740e28176a08184569149350bd8378519d659679
Flávio Zanoni <flaviozg888@gmail.com>
2024-09-20 21:21:58 -0300

    Update Chip.stories.tsx

--- src/Chip/Chip.stories.tsx
+++ src/Chip/Chip.stories.tsx
@@ -1,5 +1,5 @@
 import { Meta, StoryFn } from "@storybook/react"
-import Chip, { ChipProps } from "./Chip"
+import { Chip, ChipProps } from "./Chip"
 
 const Template: StoryFn<ChipProps> = (args) => {
   return <Chip {...args} />
